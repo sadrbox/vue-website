21159b1a59011c10516e2830b954984fb70381eb
sadrbox <sadrbox@mail.ru>
2020-05-28 18:03:58 +0600

    Website logo and title

--- src/components/layouts/content/index.vue
+++ src/components/layouts/content/index.vue
@@ -4,13 +4,9 @@
       <div class="row">
         <div class="col-3/4">
           <div class="box">
-            <h1>Is this content left</h1>
-            <p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
+            <p>Сайт рыбатекст поможет дизайнеру, верстальщику, вебмастеру сгенерировать несколько абзацев более менее осмысленного текста рыбы на русском языке, а начинающему оратору отточить навык публичных выступлений в домашних условиях. При создании генератора мы использовали небезизвестный универсальный код речей. Текст генерируется абзацами случайным образом от двух до десяти предложений в абзаце, что позволяет сделать текст более привлекательным и живым для визуально-слухового восприятия.
 
-            The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for
-            those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et
-            Malorum" by Cicero are also reproduced in their exact original form, accompanied
-            by English versions from the 1914 translation by H. Rackham.</p>
+            По своей сути рыбатекст является альтернативой традиционному lorem ipsum, который вызывает у некторых людей недоумение при попытках прочитать рыбу текст. В отличии от lorem ipsum, текст рыба на русском языке наполнит любой макет непонятным смыслом и придаст неповторимый колорит советских времен.</p>
 
           </div>
         </div>
--- src/components/layouts/header/index.vue
+++ src/components/layouts/header/index.vue
@@ -4,7 +4,13 @@
       <div class="row">
         <div class="col-3/4">
           <div class="box">
-            <img src="@/assets/img/logo.png" alt="">
+            <div class="header-website">
+              <img class="header-website__logo" src="@/assets/img/logo.png" alt="">
+              <div class="header-website__name">
+                <h1 class="header-website__name__title">Отдел образования <br/> Толебийского района</h1>
+                <h5 class="header-website__name__subtitle">официальный интернет-ресурс</h5>
+              </div>
+            </div>
           </div>
         </div>
         <div class="col-1/4">
@@ -16,3 +22,31 @@
 </template>
 <script>
 </script>
+<style lang="scss" scoped>
+.header-website {
+  display:flex;
+}
+.header-website__logo {
+  height: 100px;
+  width: auto;
+}
+.header-website__name {
+  display:flex;
+  flex-direction:column;
+  justify-content:center;
+  //line-height: 2rem;
+  padding-left:20px;
+}
+.header-website__name__title {
+  text-transform:uppercase;
+  font-size:22pt;
+  line-height:2.4rem;
+  //font-weight:bold;
+}
+.header-website__name__subtitle {
+  //text-align:center;
+  color: #777;
+  font-size: 10pt;
+  text-transform:uppercase;
+}
+</style>
