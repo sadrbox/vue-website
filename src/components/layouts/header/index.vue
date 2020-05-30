1f8adf6cdc00f67c508590fdfb77c96a72bb5d71
sadrbox <sadrbox@mail.ru>
2020-05-30 16:26:50 +0600

    font sass mixin and logo header

--- src/components/layouts/header/index.vue
+++ src/components/layouts/header/index.vue
@@ -2,17 +2,16 @@
   <div id="header">
     <div class="container">
       <div class="row">
-        <div class="col-xl-2 col-l-2 col-m-2 col-s-12 col-xs-12">
+        <div class="col-xl-2 col-l-2 col-m-3 col-s-12 col-xs-12">
           <div class="header__logo">
             <img class="header__logo__img" src="@/assets/img/logo.png" alt="">
           </div>
         </div>
-        <div class="col-xl-6 col-l-6 col-m-6 col-s-12 col-xs-12">
-          <div class="header-top">
-            <div class="header-website__name">
-              <h1 class="header-website__name__title">Отдел образования <br/> Толебийского района</h1>
-              <h5 class="header-website__name__subtitle">официальный интернет-ресурс</h5>
-            </div>
+        <div class="col-xl-6 col-l-6 col-m-5 col-s-12 col-xs-12">
+          <div class="header__name">
+            <h1 class="header__name__title">Отдел образования <br/> Толебийского района</h1>
+            <hr class="header__name__border" />
+            <h5 class="header__name__subtitle">официальный интернет-ресурс</h5>
           </div>
         </div>
         <div class="col-xl-2 col-l-4 col-m-4">
@@ -26,35 +25,27 @@
 <script>
 </script>
 <style lang="scss" scoped>
-.header-top {
+@import "@/assets/scss/_variable";
+
+.header__logo {
   display:flex;
+  align-items: center;
 }
-.header-top {
-  //height: 100px;
-  //width: auto;
+.header__logo__img {
+
 }
-.header-top__logo {
+.header__name {
   display:flex;
-  //flex-direction:column;
-  //justify-content:center;
-  //line-height: 2rem;
-  //padding-left:20px;
-  //width: 128px;
+  flex-direction: column;
+  justify-content: center;
 }
-.header-top__logo__img {
-  //height: 128px;
-  //width: 128px;
-}
-.header-website__name__title {
+.header__name__title{
+  @include font(2.4rem, bold);
   text-transform:uppercase;
-  font-size:22pt;
-  line-height:2.4rem;
-  //font-weight:bold;
 }
-.header-website__name__subtitle {
-  //text-align:center;
-  color: #777;
-  font-size: 10pt;
+.header__name__subtitle {
+  @include font(1rem);
+  color: #444;
   text-transform:uppercase;
 }
 </style>
