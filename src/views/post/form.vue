e3cea8277dc3371a2e313e7409e56c87e346b9d3
sadrbox <sadrbox@mail.ru>
2020-06-16 15:34:11 +0600

    Вт июн 16 15:34:11 +06 2020

--- src/views/post/form.vue
+++ src/views/post/form.vue
@@ -1,26 +1,22 @@
 ﻿<template>
   <div>
-    <div class="form-group">
-      <div class="form-group__label">
-        <label for="label-name">Заголовок</label>
-      </div>
-      <div class="form-group__field">
-        <input type="text" name="label-name" />
-      </div>
+    <div class="field">
+      <label for="label-name">Заголовок</label>
+      <input type="text" name="label-name" />
     </div>
   </div>
 </template>
 <style lang="scss" scoped>
-.form-group {
-  .form-group__label {
-    label {}
-  }
-  .form-group__field {
-    input {
-      //border-radius: 2px;
-      //border:2px solid #666;
-      //line-height: 20px;
-    }
+.field {
+  label {}
+  input {
+    line-height: 1.21428571em;
+    padding: .67857143em 1em;
+    font-size: 1em;
+    background: #fff;
+    border: 1px solid rgba(34,36,38,.15);
+    color: rgba(0,0,0,.87);
+    border-radius: .28571429rem;  
   }
 }
 </style>
